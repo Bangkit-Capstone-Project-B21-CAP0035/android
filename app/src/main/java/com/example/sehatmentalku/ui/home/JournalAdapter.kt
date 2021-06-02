07e3eb3c14434c0ee1599185c29579a69a9c89ba
Anwarul Fattach <emosekolah@gmail.com>
2021-06-02 18:58:26 +0700

    prepare activity detail done

--- app/src/main/java/com/example/sehatmentalku/ui/home/JournalAdapter.kt
+++ app/src/main/java/com/example/sehatmentalku/ui/home/JournalAdapter.kt
@@ -1,18 +1,22 @@
 package com.example.sehatmentalku.ui.home
 
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.sehatmentalku.R
 import com.example.sehatmentalku.data.model.Journal
-import com.example.sehatmentalku.data.model.JournalItem
+import com.example.sehatmentalku.ui.journal.JournalActivity
 
-class JournalAdapter(private val journalList: MutableList<Journal?>?) : RecyclerView.Adapter<JournalAdapter.JournalViewHolder>() {
+class JournalAdapter(
+    private val journalList: MutableList<Journal?>?
+    ) : RecyclerView.Adapter<JournalAdapter.JournalViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JournalViewHolder {
         val journalInflater = LayoutInflater.from(parent.context).inflate(R.layout.journal_item,
         parent, false)
@@ -22,7 +26,6 @@ class JournalAdapter(private val journalList: MutableList<Journal?>?) : Recycler
 
     override fun onBindViewHolder(holder: JournalViewHolder, position: Int) {
         val currentItem = journalList?.get(position)
-        Log.d("LOG", "ini currentItem = " + currentItem.toString())
 
         //currentItem.image
         Glide.with(holder.img.context).load("https://i.stack.imgur.com/eJbuH.png?s=128").into(holder.img)
@@ -32,9 +35,28 @@ class JournalAdapter(private val journalList: MutableList<Journal?>?) : Recycler
 
     override fun getItemCount() = journalList!!.size
 
-    class JournalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class JournalViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+    View.OnClickListener{
         val img: ImageView = itemView.findViewById(R.id.journal_img)
         val tanggal: TextView = itemView.findViewById(R.id.journal_tanggal)
         val story: TextView = itemView.findViewById(R.id.journal_story)
+        val intent = Intent(itemView.context, JournalActivity::class.java).apply {}
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = absoluteAdapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                val currentItem = journalList?.get(position)
+                openDetail(currentItem!!)
+            }
+        }
+
+        fun openDetail (journal: Journal) {
+            Log.d("TAG", "INI terckilcka = " + journal.story)
+            startActivity(itemView.context, intent, null)
+        }
     }
 }
---
+++ app/src/main/java/com/example/sehatmentalku/ui/journal/JournalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sehatmentalku.ui.journal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sehatmentalku.R
+
+class JournalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_journal)
+    }
+}
